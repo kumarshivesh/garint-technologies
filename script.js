8363c920ad6bc46c613ab7069476dcfd877c8ece
Kumar Shivesh <116376181+kumarshivesh@users.noreply.github.com>
2024-05-12 08:44:58 +0530

    Added all files and folders

---
+++ script.js
@@ -0,0 +1,18 @@
+// <---START: `Hamburger Menu` Logic--->
+const hamburger = document.querySelector('.hamburger')
+const ul = document.querySelector('.header-container ul')
+const iframe = document.querySelector('iframe')
+
+hamburger.addEventListener('click', ()=> {
+  ul.classList.toggle('ham_active')
+
+  let ham_child = hamburger.firstElementChild
+  if(ul.classList.contains('ham_active')) {
+    ham_child.classList.replace('fa-bars', 'fa-xmark')
+    // ham_child.src = "./xmark-solid.svg"; // Change the source to the xmark icon
+  } else {
+    ham_child.classList.replace('fa-xmark', 'fa-bars')
+    // ham_child.src = "./bars-solid.svg"; // Change the source back to the bars icon
+  }
+})
+// <---END: `Hamburger Menu` Logic--->
